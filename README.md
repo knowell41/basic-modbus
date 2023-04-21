34faaa10230f04ca5d3c55f39b12703c2df88187
Emman <emmanuel_pagayonan_28@yahoo.com>
2023-04-21 22:11:43 +0800

    Dev (#1)
    * dev
    
    * dev
    
    * dev
    
    * dev

--- README.md
+++ README.md
@@ -1,1 +1,54 @@
 # basic-modbus
+
+This project is aims to explore functionality of existing python modbus implementation [pymodbus](https://pymodbus.readthedocs.io/en/latest/index.html).
+
+# Demo
+
+![demo](https://github.com/knowell41/basic-modbus/blob/dev/Demo.mp4)
+
+# Sample code
+```
+# polling_example.py
+
+from basic_modbus.modbus_poll import ModbusPoll, ModbusPollError
+import traceback
+import time
+
+"""
+Sends modbus request to read coils of each RTU,
+"""
+
+
+server = ModbusPoll("COM8", scan_rate=0, timeout=0.5)
+server.add_rtu("device_1", 1, 0, 4)
+# server.add_rtu("device_2",2,0,4)
+
+server.__enter__() # start pollin rtu
+prev = None        # initial value
+
+# loop forever
+# coil_state = False
+while True:      
+    try:
+        current_readings = server.rtu_readings.copy()
+        if prev != current_readings:
+            print(current_readings)
+            prev = current_readings.copy()
+
+        # wacth for failed modbus poll
+        # if server.error:
+        #     print(server.error)
+        #     server.error = None
+
+        # blocking function to write state to coil
+        # server.write_coil(address=0, value=coil_state, slave_id=1)
+
+        # invert coil_state to make it blink
+        # coil_state = not coil_state
+
+    except (ModbusPollError, Exception) as e:
+       server.__exit__() 
+       break
+
+
+```
---
+++ basic_modbus/modbus_poll.py
@@ -0,0 +1,139 @@
+
+
+import serial
+import time
+import traceback
+from pymodbus.client.mixin import ModbusClientMixin
+from pymodbus.client import ModbusSerialClient
+from pymodbus.exceptions import ModbusException, NoSuchSlaveException, ModbusIOException
+import time
+import threading
+import logging
+
+
+
+WRITE_SINGLE_COIL = 5
+WRITE_SINGLE_REGISTER = 6
+WRITE_COILS = 15
+WRITE_REGISTERS = 16
+REPORT_SERVER_ID = 17
+READ_COILS = 1
+
+class ModbusPollError(Exception):
+    pass
+
+class ModbusPoll:
+    def __init__(self, port:str, baudrate:int=9600, timeout:float=0.5, scan_rate:float=0.5):
+        """**ModbusPoll**
+        :param port: Serial port used for communication.
+        :param baudrate: (optional) Bits per second.
+        :param timeout: (optional) serial timeout.
+        :param scan_rate: (optional) polling delay.
+        """
+        self.port = port
+        self.baudrate = baudrate
+        self.timeout = timeout
+        self.scan_rate = scan_rate
+
+        
+        self.rtu = []
+        self.rtu_readings = []
+        self.running = False
+        self.client = ModbusSerialClient(self.port, baudrate=self.baudrate, timeout=self.timeout)
+        self.write_to_coil = None
+        self.error = None
+
+    def disconnect_serial(self):
+        """**disconnecT_serial**
+        Disconnect to serial.
+        """
+        if self.client.connected:
+            self.client.close()
+
+    def connect_serial(self):
+        """**connect_serial**
+        Connect to serial.
+        """
+        if not self.client.connected:
+            self.client.connect()
+
+    def add_rtu(self, rtu_name:str, slave_id:int, address:int, quantity:int):
+        """**add_rtu**
+        Adds RTU to poll.
+        :param rtu_name: Name of the RTU.
+        :param slave_id: Slave ID.
+        :param address: Coil start address.
+        :param quantity: number of coils.
+        """
+        for dev in self.rtu:
+            if dev.get("slave_id") == slave_id:
+                return False, f"Unable to add RTU with slave_id {slave_id}."
+            
+        self.rtu.append({
+            "name": rtu_name,
+            "slave_id": slave_id,
+            "address": address,
+            "quantity": quantity
+        })
+        return True, f"RTU with slave_id {slave_id} added successfully."
+    
+    def write_coil(self, address:int, value:bool, slave_id:int)->None:
+        """**write_coil**
+        Blocking function to write coil state
+        """
+        self.write_to_coil = (address, value, slave_id)
+        while self.write_to_coil != None:
+            pass
+
+    def poll(self):
+        """**poll**
+        Polling registered rtu with frequency defined by scan_rate
+        """
+
+        self.connect_serial()
+        cmd = dict()
+        for rtu in self.rtu:
+            cmd[rtu["name"]] = (rtu["address"], rtu["quantity"], rtu["slave_id"])
+        last_readings = []
+        while self.running:
+            response = {}
+            for key in cmd.keys():
+                try:
+                    last_readings =  self.client.read_coils(cmd[key][0], cmd[key][1],cmd[key][2] ).bits
+                    response[key] = last_readings
+                    self.error = None
+                except Exception as e:
+                    response[key] = last_readings
+                    self.error = e
+                    
+                    
+            self.rtu_readings = response.copy()
+            if self.write_to_coil:
+                self.client.write_coil(self.write_to_coil[0], self.write_to_coil[1], self.write_to_coil[2])
+                self.write_to_coil = None
+            time.sleep(self.scan_rate)
+
+    def __enter__(self):
+        """
+        start polling
+        """
+        if not self.running:
+            self.running = True
+            self.poll_thread = threading.Thread(target=self.poll, daemon=True)
+            self.poll_thread.start()
+            print("modbus poll started")
+        else:
+            print("modbus poll is already running")
+
+    def __exit__(self):
+        """
+        end polling
+        """
+        if self.running:
+            self.running = False
+            self.poll_thread.join()
+            self.rtu_readings = dict()
+            self.disconnect_serial()
+            print("modbus poll stop")
+        else:
+            print("modbus poll is not running.")
